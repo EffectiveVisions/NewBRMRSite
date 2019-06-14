93c67915c60884198e636527a9b1d5ec0341dc71
Anku <anku@rudrainnovative.com>
2019-06-14 13:19:50 +0530

    Worked on mobile menu

--- themes/efora-child/js/custom.js
+++ themes/efora-child/js/custom.js
@@ -2,22 +2,22 @@ AOS.init();
 jQuery(window).load(function() {
 
 setTimeout(function(){
-if(navigator.userAgent.indexOf("Speed Insights") == -1) { 
+  if(navigator.userAgent.indexOf("Speed Insights") == -1) { 
       var js = ["/wp-content/themes/efora-child/js/tp.widget.bootstrap.min.js"];
       var $head = jQuery("head");
       for (var i = 0; i < js.length; i++) {
 	$head.append("<script defer='true' src=\"" + js[i] + "\"></scr" + "ipt>");
       }
-}
+  }
 }, 6000)
 
 if(jQuery('.frm_error_style')[0] || jQuery('.frm_message')[0]){
-setTimeout(function(){
-   jQuery('html, body').animate({
-      scrollTop: jQuery("#contactus").offset().top-150
-   });        
- },500); 
-}            
+    setTimeout(function(){
+       jQuery('html, body').animate({
+          scrollTop: jQuery("#contactus").offset().top-150
+       });        
+     },500); 
+ }            
 });
 
 jQuery(document).ready(function(){              
@@ -46,10 +46,11 @@ jQuery(document).ready(function(){
   	 jQuery("body").append(jQuery("#ui-datepicker-div"));
    });
 
-  jQuery(".mainmenu").css({"background":"#005a9a","margin-top":"70px"})
+  jQuery(".mainmenu").css({"background":"#005a9a","margin-top":"15px"})
   jQuery(".mainmenu").css({"color":"#fff"})
   jQuery(".community").css({"padding-left":"20px"});
   jQuery(".community").css({"background":"rgb(237, 241, 243)"});
   jQuery(".mainmenu").children("a").css({"color":"#fff","font-weight":"bold"});
+
   //jQuery(".closemenu").next().find("a").css({"color":"#fff"});
 });
