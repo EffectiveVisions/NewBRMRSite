1652d6343124a0516cfad0eda6b116d80a0959dd
Anku <anku@rudrainnovative.com>
2019-06-19 13:54:31 +0530

    Worked on mobile search

--- themes/efora-child/js/custom.js
+++ themes/efora-child/js/custom.js
@@ -86,9 +86,17 @@ jQuery(document).ready(function(){
     setTimeout(function(){
        jQuery(".homepagesearch").find(".guests-sum").html("");
        jQuery(".homepagesearch").find(".guests-sum-label").html("Guest");
+       jQuery(".homepagesearch").find(".adults_label").html("Adult");
+       jQuery(".homepagesearch").find(".children-label").html("Child");
        jQuery(".searchbyarea-banner").find(".guests-sum").html("");
        jQuery(".searchbyarea-banner").find(".guests-sum-label").html("Guest");
-    },500)
+       jQuery(".searchbyarea-banner").find(".adults_label").html("Adult");
+       jQuery(".searchbyarea-banner").find(".children-label").html("Child");
+       jQuery(".homepagesearch").find(".adultscount").html("");
+       jQuery(".homepagesearch").find(".children-count").html("");
+       jQuery(".searchbyarea-banner").find(".adultscount").html("");
+       jQuery(".searchbyarea-banner").find(".children-count").html("");
+    },200)
       
       
   });
@@ -97,27 +105,104 @@ jQuery(document).ready(function(){
       setTimeout(function(){
          jQuery(".homepagesearch").find(".guests-sum").html("");
          jQuery(".homepagesearch").find(".guests-sum-label").html("Guest");
+         jQuery(".homepagesearch").find(".adults_label").html("Adult");
+         jQuery(".homepagesearch").find(".children-label").html("Child");
          jQuery(".searchbyarea-banner").find(".guests-sum").html("");
          jQuery(".searchbyarea-banner").find(".guests-sum-label").html("Guest");
-       },500)
+          jQuery(".homepagesearch").find(".adultscount").html("");
+         jQuery(".homepagesearch").find(".children-count").html("");
+         jQuery(".searchbyarea-banner").find(".adultscount").html("");
+         jQuery(".searchbyarea-banner").find(".children-count").html("");
+         jQuery(".searchbyarea-banner").find(".adults_label").html("Adult");
+         jQuery(".searchbyarea-banner").find(".children-label").html("Child");
+       },200)
   });
 
   jQuery(".increase-child").on("click",function(){
        setTimeout(function(){
          jQuery(".homepagesearch").find(".guests-sum").html("");
          jQuery(".homepagesearch").find(".guests-sum-label").html("Guest");
+         jQuery(".homepagesearch").find(".adults_label").html("Adult");
+         jQuery(".homepagesearch").find(".children-label").html("Child");
          jQuery(".searchbyarea-banner").find(".guests-sum").html("");
          jQuery(".searchbyarea-banner").find(".guests-sum-label").html("Guest");
-       },500)
+         jQuery(".searchbyarea-banner").find(".adults_label").html("Adult");
+         jQuery(".searchbyarea-banner").find(".children-label").html("Child");
+         jQuery(".homepagesearch").find(".adultscount").html("");
+         jQuery(".homepagesearch").find(".children-count").html("");
+         jQuery(".searchbyarea-banner").find(".adultscount").html("");
+         jQuery(".searchbyarea-banner").find(".children-count").html("");
+       },200)
   });
 
   jQuery(".decrease-child").on("click",function(){
       setTimeout(function(){
          jQuery(".homepagesearch").find(".guests-sum").html("");
          jQuery(".homepagesearch").find(".guests-sum-label").html("Guest");
+         jQuery(".homepagesearch").find(".adults_label").html("Adult");
+         jQuery(".homepagesearch").find(".children-label").html("Child");
          jQuery(".searchbyarea-banner").find(".guests-sum").html("");
          jQuery(".searchbyarea-banner").find(".guests-sum-label").html("Guest");
-       },500)
+         jQuery(".homepagesearch").find(".adultscount").html("");
+         jQuery(".homepagesearch").find(".children-count").html("");
+         jQuery(".searchbyarea-banner").find(".adultscount").html("");
+         jQuery(".searchbyarea-banner").find(".children-count").html("");
+       },200)
+  });
+
+  jQuery(".rounded-circle").one("click", function(){
+     if(jQuery(this).hasClass( "increase-adult") || jQuery(this).hasClass("decrease-adult") || jQuery(this).hasClass("decrease-child") || jQuery(this).hasClass("increase-child")){
+       
+       jQuery(".homepagesearch").find(".pets_count").val("");
+       jQuery(".homepagesearch").find(".pets_count").trigger('input') ;// Use for Chrome/Firefox/Edge
+       jQuery(".homepagesearch").find(".pets_count").trigger('change');
+       jQuery(".homepagesearch").find("#guestsDropClearBtn").trigger("click");
+       jQuery(".searchbyarea-banner").find("#guestsDropClearBtn").trigger("click");
+       jQuery(".searchbyarea-banner").find(".pets_count").val("");
+       jQuery(".searchbyarea-banner").find(".pets_count").trigger('input') ;// Use for Chrome/Firefox/Edge
+       jQuery(".searchbyarea-banner").find(".pets_count").trigger('change');
+     }else{
+         jQuery(".mobile-nav-calender").find("#guestsDropClearBtn1").trigger("click");
+         jQuery(".mobile-nav-calender").find(".pets_count").val("");
+         jQuery(".mobile-nav-calender").find(".pets_count").trigger('input');
+         jQuery(".mobile-nav-calender").find(".pets_count").trigger('change');
+     }
+  });
+
+  jQuery(".rounded-circle").on("click",function(){
+
+     if(jQuery(this).hasClass( "increase-adult") || jQuery(this).hasClass("decrease-adult") || jQuery(this).hasClass("decrease-child") || jQuery(this).hasClass("increase-child")){
+          
+     }else{
+        setTimeout(function(){
+            var homeadultscount   = parseInt(jQuery(".homepagesearch").find(".adultscount").html());
+            var homechildrencount = parseInt(jQuery(".homepagesearch").find(".children-count").html());
+            var searchadultscount = parseInt(jQuery(".searchbyarea-banner").find(".adultscount").html());
+            var searchchildcount  = parseInt(jQuery(".searchbyarea-banner").find(".children-count").html());
+            if(homeadultscount && homeadultscount>1){
+                 jQuery(".homepagesearch").find(".adults_label").html("Adults");
+            }else{
+                 jQuery(".homepagesearch").find(".adults_label").html("Adult");
+            }
+            if(homechildrencount && homechildrencount>1){
+                jQuery(".homepagesearch").find(".children-label").html("Children");
+            }else{
+                jQuery(".homepagesearch").find(".children-label").html("Child");
+            }
+            if(searchadultscount && searchadultscount>1){
+                jQuery(".searchbyarea-banner").find(".adults_label").html("Adults");
+            }else{
+                jQuery(".searchbyarea-banner").find(".adults_label").html("Adult");
+            }
+            if(searchchildcount && searchchildcount>1){
+                jQuery(".searchbyarea-banner").find(".children-label").html("Children");
+            }else{
+                jQuery(".searchbyarea-banner").find(".children-label").html("Child");
+            }
+            jQuery(".mobile-nav-calender").find(".guests-sum").html("");
+            jQuery(".mobile-nav-calender").find(".guests-sum-label").html("Guest");
+        },200)
+     }
   });
 
 });
