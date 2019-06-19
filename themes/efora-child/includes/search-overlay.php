7c80b5d5689c810c005ea2205d95b24b0e3ea189
Anku <anku@rudrainnovative.com>
2019-06-19 14:54:03 +0530

    Removing search popup

--- themes/efora-child/includes/search-overlay.php
+++ themes/efora-child/includes/search-overlay.php
@@ -8,18 +8,18 @@
             </div>
         </fieldset>
     </form>-->
-    <div class="search">
+    <!--<div class="search">
         <div ng-controller='PlusMinusControler as pCtrl' class="input-group">
-          <div class="input-group-text p-0 border-0 rounded-0 d-none">
+          <div class="input-group-text p-0 border-0 rounded-0">
             <span class="input-group-text border-0 cursor-pointer close-search-btn" style=" background-color: #efefef;" id="basic-addon1"><i class="icon icon-search font-20"></i></span>
           <input id="searchbox" type="text" class="form-control pr-3 font-Nunito font-weight-normal" ng-model='area' 
           uib-typeahead='area as area.name for area in locations | filter:$viewValue:stateComparator' 
           typeahead-on-select='onSearchSelect(area.id)' typeahead-focus-first="true" placeholder="Search Location" aria-label="Search" aria-describedby="basic-addon1">
           </div>
         </div>
-    </div>
+    </div>-->
 
-    <div class="selectsearch">
+    <div class="selectsearch d-none">
      <?php if (is_active_sidebar('headersearch')): ?>
          <?php dynamic_sidebar('headersearch'); ?>
     <?php endif; ?>
