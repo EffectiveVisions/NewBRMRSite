0e455d71d1896524609c6040ef496b36c858cad1
Anku <anku@rudrainnovative.com>
2019-06-24 11:27:21 +0530

    Worked on lazy loading image

--- themes/efora-child/functions.php
+++ themes/efora-child/functions.php
@@ -229,10 +229,20 @@ function add_file_types_to_uploads($file_types) {
 
 add_action('upload_mimes', 'add_file_types_to_uploads');
 
+
+function my_custom_js() {
+
+    $url = get_stylesheet_directory_uri() . '/js/picturefill.min.js';
+
+    echo '<script type="text/javascript" src='.$url.' async></script>';
+}
+
+add_action( 'wp_head', 'my_custom_js' );
+
 function defer_parsing_of_js($url) {
     if (FALSE === strpos($url, '.js'))
         return $url;
-    if (strpos($url, 'jquery/jquery.js') || strpos($url, 'angular.min.js') || strpos($url, 'resortpro.min.js') || strpos($url, 'aos.js') || strpos($url, 'commonangularfile.js')) {
+    if (strpos($url, 'jquery/jquery.js') || strpos($url, 'angular.min.js') || strpos($url, 'resortpro.min.js') || strpos($url, 'aos.js') || strpos($url, 'commonangularfile.js') || strpos($url, 'picturefill.min.js')) {
         return $url;
         //return "$url' async='async";
     } else {
