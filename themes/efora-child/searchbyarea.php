9fda37426465b756bf5383e91e127cbc88f261c0
Eric <onebattleman@users.noreply.github.com>
2019-07-08 15:43:06 -0400

    Changed "View" to "Mountain View" Amenity
    On Search by Date page and Search by Area.

--- themes/efora-child/searchbyarea.php
+++ themes/efora-child/searchbyarea.php
@@ -454,7 +454,7 @@ $luxury_homes_section = get_post_custom_values('luxury_homes_section',$page->ID)
       //jQuery(".amenity_item").addClass("d-none d-md-inline-block");
       //jQuery("#resortpro-search-amenities-block-not").addClass("d-none d-md-inline-block");
       jQuery(".amenity_item").each(function(){
-          if(jQuery(this).children("input").val() == "121857" || jQuery(this).children("input").val() == "121865" || jQuery(this).children("input").val() == "416500" || jQuery(this).children("input").val() == "121870"){ 
+          if(jQuery(this).children("input").val() == "121857" || jQuery(this).children("input").val() == "121865" || jQuery(this).children("input").val() == "121866" || jQuery(this).children("input").val() == "121870"){ 
 
             if(jQuery(this).children("input").val() == "121870"){
                 jQuery("input[value='121870']").css({"vertical-align":"middle","outline":"none"});
@@ -475,8 +475,8 @@ $luxury_homes_section = get_post_custom_values('luxury_homes_section',$page->ID)
                jQuery("input[value='121865']").after('<img class="ammenityicon w-18" src="/wp-content/uploads/2019/06/pawprint.svg">');
             }
 
-            if(jQuery(this).children("input").val() == "416500"){
-              jQuery("input[value='416500']").after('<img class="ammenityicon" src="/wp-content/uploads/2019/06/Mountain.svg">');
+            if(jQuery(this).children("input").val() == "121866"){
+              jQuery("input[value='121866']").after('<img class="ammenityicon" src="/wp-content/uploads/2019/06/Mountain.svg">');
             }
             
             jQuery(this).children("input").css({"vertical-align":"middle","outline":"none"});
